c85a9c005118a0ecf2f23b140d06bc636becd89e
shehab-nabil <shehabnabi@gmail.com>
2023-06-27 18:19:42 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NoteApp());
+}
+
+class NoteApp extends StatelessWidget {
+  const NoteApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      home: Scaffold(),
+    );
+  }
+}
